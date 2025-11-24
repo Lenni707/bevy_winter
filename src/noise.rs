37cef6f8109529126c8764f9d3f6e6a49b2d188b
Lenni <gamer_like_you@gmx.de>
2025-11-24 21:42:17 +0100

    idk what i changed

--- src/noise.rs
+++ src/noise.rs
@@ -1,9 +1,9 @@
-use noise::{Perlin, RidgedMulti, Seedable};
+use noise::{Perlin, Seedable};
 use bevy::prelude::*;
 
 #[derive(Resource)]
 pub struct NoiseGenerators {
-    pub height: RidgedMulti,
+    pub height: Perlin,
     pub biome: Perlin,
     pub river: Perlin,
 }
@@ -11,7 +11,7 @@ pub struct NoiseGenerators {
 impl NoiseGenerators {
     pub fn new(seed: u32) -> Self {
         Self {
-            height: RidgedMulti::new().set_seed(seed), 
+            height: Perlin::new().set_seed(seed), 
             biome: Perlin::new().set_seed(seed + 69),
             river: Perlin::new().set_seed(seed + 420),
         }
